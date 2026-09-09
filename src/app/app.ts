72e5fa23cf2ba71c0529f890c5579f55976cec5f
Vitor Silva <almeidadasilvavitorhugo525@gmail.com>
2026-09-09 17:46:23 -0300

    Create app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { ClienteCadastrar } from './components/cliente/cliente-cadastrar/cliente-cadastrar';
+
+@Component({
+  selector: 'app-root',
+  imports: [ClienteCadastrar],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('formulario-cliente');
+}
